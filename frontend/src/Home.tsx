8518a699a5a2f4f33e37a103ecb3feca8c723f28
Ajith k v <ajith66310@gmail.com>
2026-01-02 18:05:31 +0530

    fix: add responsive SVGs for desktop and mobile views with visibility control

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -132,14 +132,43 @@ const Home = () => {
         </div>
 
         <div className="svg-path">
-          <svg width="1554" height="3791" viewBox="0 0 1554 3791" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              id="stroke-path"
+  {/* Desktop SVG */}
+  <svg
+    className="svg-desktop"
+    width="1554"
+    height="3791"
+    viewBox="0 0 1554 3791"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      id="stroke-path"
+      d="M707.54 166.337C707.54 166.337 87.0399 27.3369 167.04 1051.34C247.041 2075.34 1047.04 955.337 1359.04 2059.34C1671.04 3163.34 -227.58 3009.5 308.42 2145.5C844.42 1281.5 909.04 3630.34 909.04 3630.34"
+      stroke="#F6D3BD"
+      strokeWidth="320"
+      strokeLinecap="round"
+    />
+  </svg>
 
-              d="M707.54 166.337C707.54 166.337 87.0399 27.3369 167.04 1051.34C247.041 2075.34 1047.04 955.337 1359.04 2059.34C1671.04 3163.34 -227.58 3009.5 308.42 2145.5C844.42 1281.5 909.04 3630.34 909.04 3630.34" stroke="#F6D3BD" stroke-width="320" stroke-linecap="round" />
-          </svg>
+  {/* Mobile SVG */}
+  <svg
+    className="svg-mobile"
+    width="1554"
+    height="4720"
+    viewBox="0 0 1554 4720"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      id="stroke-path"
+      d="M707.54 166.337C707.54 166.337 87.0399 27.3369 167.04 1051.34C247.041 2075.34 1047.04 955.337 1359.04 2059.34C1671.04 3163.34 -227.58 3009.5 308.42 2145.5C844.42 1281.5 813.92 4560 813.92 4560"
+      stroke="#F6D3BD"
+      strokeWidth="320"
+      strokeLinecap="round"
+    />
+  </svg>
+</div>
 
-        </div>
       </section>
 
       <section className="outro">
